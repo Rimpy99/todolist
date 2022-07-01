eae4f70602cb16d467898772b5902fc95033dac9
Rimpy99 <70006pawel@gmail.com>
2022-07-01 21:33:28 +0200

    Animations added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import {useState} from 'react';
 import './App.css';
 import './App-responsive.css';
 import { SiAddthis } from "react-icons/si";
+import { motion } from 'framer-motion';
+
+//INTERFACES FOR TYPESCRIPT
 
 interface TaskTemplate {
   taskName: string;
@@ -19,6 +22,65 @@ interface CategoryTemplate {
 interface Functions {
   deleteTask: (id: string)=>void;
 }
+
+//VARIANTS FOR ANIMATIONS
+
+const headerH1Variants = {
+  hidden: { y: "-100vh" },
+  visible: { 
+    y: 0,
+    transition: { 
+      ease: "easeInOut",
+      duration: 1.5, 
+    }
+  },
+}
+
+const inputVariants = {
+  hidden: { x: "-100vw" },
+  visible: { 
+    x: 0,
+    transition: { 
+      duration: .5, 
+    }
+  },
+}
+
+const addBtnVariants = {
+  hidden: { x: "100vw" },
+  visible: { 
+    x: 0,
+    transition: { 
+      duration: .5, 
+    }
+  },
+}
+
+const categoryVariants = {
+  hidden: {
+    y: "100vh",
+    opacity: 0, 
+  },
+  visible: { 
+    y: 0,
+    opacity: 1,
+    transition: { 
+      duration: 1.2, 
+    }
+  },
+}
+
+const sortContainerVaraints = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { 
+      delay: 1.5,
+    }
+  }
+}
+
+//MAIN APP
 
 const App: React.FC = () =>{
 
@@ -74,13 +136,32 @@ const App: React.FC = () =>{
   return (
     <div className="app-container">
       <header>
-        <h1>TO DO LIST</h1>
+        <motion.h1
+          variants={headerH1Variants}
+          initial="hidden"
+          animate="visible"
+        >
+          TO DO LIST
+        </motion.h1>
       </header>
       <div className="task-creator">
         <div className="task-creator__input-container">
-          <input type="text" value={newTaskName} placeholder="Add a task" maxLength={35} className="task-creator__input" onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setNewTaskName(event.target.value)}/>
-          <div className={inputIsChecked && (newTaskName !== "") ? "" : "input-not-checked"}>
-            <SiAddthis size="55" color="rgb(2, 0, 114)" cursor="pointer"
+          <motion.input 
+            type="text" 
+            value={newTaskName} 
+            placeholder="Add a task" 
+            maxLength={35} 
+            className="task-creator__input" 
+            variants={inputVariants}
+            initial="hidden"
+            animate="visible"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>)=>setNewTaskName(event.target.value)}
+          />
+          <motion.div className={inputIsChecked && (newTaskName !== "") ? "" : "input-not-checked"} variants={addBtnVariants} initial="hidden" animate="visible" whileHover={{scale:1.1}}>
+            <SiAddthis 
+              size="55" 
+              color="rgb(2, 0, 114)" 
+              cursor="pointer"
               onClick={()=>{
                 setTasks((current)=>[...current,{
                   taskName: newTaskName,
@@ -92,25 +173,28 @@ const App: React.FC = () =>{
                 setNewTaskName("");
               }}
             />
-          </div>
+          </motion.div>
         </div>
         <div className="task-creator__categories-container">
           {categoryInputs.map(({categoryName, categoryId})=>{
             return (
-              <div>
+              <motion.div variants={categoryVariants} initial="hidden" animate="visible" whileHover={{scale: 1.1}}>
                 <input type="radio" name="category-input" value={categoryName} id={`${categoryId}`} className={`category-input input-${categoryId}`} onChange={()=>setInputIsChecked(true)} onClick={()=>setNewTaskCategory({
                   categoryName: categoryName,
                   categoryId: categoryId,
                 })}/>
-                <label htmlFor={`${categoryId}`} className={`category-label category-${categoryId}`}>{categoryName}</label>
-              </div>
+                <motion.label 
+                  htmlFor={`${categoryId}`} 
+                  className={`category-label category-${categoryId}`}
+                >{categoryName}</motion.label>
+              </motion.div>
             )
           })}
         </div>
       </div>
-      <div className="sort-container">
-        <button className="sort-btn" onClick={()=>sortTable()}>Sort by priority</button>
-      </div>
+      <motion.div className="sort-container" variants={sortContainerVaraints} initial="hidden" animate="visible">
+        <motion.button className="sort-btn" onClick={()=>sortTable()} whileHover={{scale: 1.1}} transition={{type: "easeInOut", duration: .2}}>Sort by priority</motion.button>
+      </motion.div>
       <div className="task-container">
         {tasks.map(taskData=>{
           return <Task taskData={taskData} taskDeleteFunction={deleteTask}/>
--- src/componenets/Task.tsx
+++ src/componenets/Task.tsx
@@ -2,6 +2,9 @@ import {useState} from 'react';
 import "./Task.css";
 import { MdDeleteForever } from "react-icons/md";
 import { BsBookmarkCheckFill } from "react-icons/bs";
+import { motion, AnimatePresence } from "framer-motion";
+
+//INTEFACES FOR TYPESCRIPT
 
 interface Props{
     taskData: TaskTemplate;
@@ -13,29 +16,53 @@ interface TaskTemplate {
     taskCategory: string;
     taskCategoryId: number;
     taskId: string;
-  }
+}
+
+//VARIANTS FOR ANIMATIONS
+
+const taskVariant = {
+    hidden: { 
+        scale: .5,
+    },
+    visible: { 
+        scale: 1,
+        transition: {
+            type: "spring",
+            stiffness: 50,
+            duration: .5,
+        }
+    },
+}
 
 const Task: React.FC<Props> = ({taskData, taskDeleteFunction}) => {
 
     const [taskIsDone, setTaskIsDone] = useState<boolean>(false);
 
     return(
-        <div className={`task ${taskIsDone ?  "task-done" : null}`}>
-            <div className="task-header">
-                <div className={`category-block category-id-${taskData.taskCategoryId}`}/>
-                <div className="task-buttons">
-                    <div className="task-buttons-container">
-                        <BsBookmarkCheckFill size="20" color="rgb(2, 0, 114)" cursor="pointer" onClick={()=>setTaskIsDone(!taskIsDone)}/>
-                    </div>
-                    <div className="task-buttons-container">
-                        <MdDeleteForever size="27" color="rgb(2, 0, 114)" cursor="pointer" onClick={()=>taskDeleteFunction(taskData.taskId)}/>
+        <AnimatePresence>
+            <motion.div 
+                className={`task ${taskIsDone ? "task-done" : null}`} 
+                variants={taskVariant}
+                initial="hidden"
+                animate="visible"
+                exit={{y: 20}}
+            >
+                <div className="task-header">
+                    <div className={`category-block category-id-${taskData.taskCategoryId}`}/>
+                    <div className="task-buttons">
+                        <div className="task-buttons-container">
+                            <BsBookmarkCheckFill size="20" color="rgb(2, 0, 114)" cursor="pointer" onClick={()=>setTaskIsDone(!taskIsDone)}/>
+                        </div>
+                        <div className="task-buttons-container">
+                            <MdDeleteForever size="27" color="rgb(2, 0, 114)" cursor="pointer" onClick={()=>taskDeleteFunction(taskData.taskId)}/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="task-name-container">
-                <h2>{taskData.taskName}</h2>
-            </div>
-        </div>
+                <div className="task-name-container">
+                    <h2>{taskData.taskName}</h2>
+                </div>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
